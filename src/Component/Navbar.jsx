9891d3f9e9384c7803aab005b9cb0b6f24a9dcf6
unknown <borshadevi27@gmail.com>
2025-02-12 13:29:06 +0600

    added navbar text

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { HiX } from "react-icons/hi";
+import { NavLink } from "react-router";
+const Navbar=()=>{
+    const [open , isOpen]=useState(false)
+    const navLink=(
+        <div className="flex flex-col md:flex-row md:mx-6 md:gap-4">
+            <NavLink to='' className='border-b-2 border-b-transparent  transition-colors duration-1000 transform  hover:border-blue-500'>Home</NavLink>
+            <NavLink to='' className='border-b-2 border-b-transparent  transition-colors duration-1000 transform  hover:border-blue-500'>About Us</NavLink>
+            <NavLink to='' className='border-b-2 border-b-transparent  transition-colors duration-1000 transform  hover:border-blue-500'>Contact Us</NavLink>
+            
+        </div>
+    )
+    return(
+        <>
+     <nav  className="relative bg-white shadow dark:bg-gray-800">
+    <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+        <div className="flex items-center justify-between">
+           <p>Name</p>
+
+           
+            <div className="flex lg:hidden">
+                <button onClick={() => isOpen(!open)}  className="text-white hover:text-white">
+                    {
+                        open ? <HiX></HiX> : <FaBarsStaggered></FaBarsStaggered>
+                    }
+                    
+                </button>
+            </div>
+        </div>
+        {
+            open ?  <div  className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-out hover:ease-in bg-white  md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+              {navLink}
+            <div className="flex justify-center md:block">
+                <p>Last</p>
+            </div>
+        </div> : ''
+        }
+    </div>
+</nav>
+        </>
+    )
+}
+export default Navbar;
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
+import Navbar from "../Component/Navbar";
+import Footer from "../Component/Footer";
 
 const Root=()=>{
     return(
-        <>
-        <h1>Root</h1>
+        <div className="container">
+        <Navbar></Navbar>
         <Outlet></Outlet>
-        </>
+        <Footer></Footer>
+        </div>
     )
 }
 export default Root;
