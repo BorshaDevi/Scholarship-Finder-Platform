76be33727f01d6af8398600bd5408f74b842d534
unknown <borshadevi27@gmail.com>
2025-02-13 13:37:32 +0600

    added texts

--- src/RouteGroups/SingIn.jsx
+++ src/RouteGroups/SingIn.jsx
@@ -1,13 +1,29 @@
 import {Link} from "react-router"
+import useAxiosPrivate from "../hooks/usePrivate"
 const SignIn=()=>{
-    const handleSignIn=(e)=>{
+    const axiosPrivate=useAxiosPrivate()
+    
+    const handleSignIn=async(e)=>{
         e.preventDefault()
+        const form=e.target
+        const email=form.email.value
+        const password=form.password.value
+        const login={
+            email,
+            password
+        }
+        console.log(login)
+        const loginUser=await axiosPrivate.post('/loginUser',login)
+        .then(res =>{
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
     }
     return(
         <>
        <div className=' mt-10  lg:mx-auto md:ml-64 mb-24 md:h-60 lg:container drop-shadow-2xl shadow-white  bg-green-300    md:w-80 md:p-3 '>
         <form onSubmit={handleSignIn} >
-              <input type="email" name='email' required className='w-full border border-black mt-5 p-1' placeholder='Enter your Name'></input>
+              <input type="email" name='email' required className='w-full border border-black mt-5 p-1' placeholder='Enter your Email'></input>
               <br />
               <input type="password" name="password" required className='w-full border border-black p-1 mt-5' placeholder='Enter your password'  ></input>
               <br />
